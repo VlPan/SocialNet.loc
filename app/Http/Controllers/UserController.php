2c811588db0b6443989e28cfc2ebc2f4986f7217
VlPan <VladimirPant.bel@gmail.com>
2017-04-24 20:55:45 +0300

    +Page urls

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 class UserController extends Controller
@@ -64,11 +66,18 @@ class UserController extends Controller
 
     public function postSaveAccount(Request $request)
     {
+//        dump($request);
         $this->validate($request, [
-            'first_name' => 'required|max:120'
+            'first_name' => 'required|max:120',
+            'second_name' => 'required|max:120'
         ]);
         $user = Auth::user();
         $old_name = $user->first_name;
+        $user->second_name = $request['second_name'];
+        $user->city = $request['city'];
+        $user->country = $request['country'];
+        $user->gender = $request['gender'];
+        $user->status = $request['status'];
         $user->first_name = $request['first_name'];
         $user->update();
         $file = $request->file('image');
@@ -93,4 +102,55 @@ class UserController extends Controller
         $file = Storage::disk('local')->get($filename);
         return new Response($file,200);
     }
+
+    public function getPage($id)
+    {
+
+        $user = Auth::user();
+        if ($id == $user->id) {
+            $posts = DB::table('posts')->where('user_id', $user->id)->get();
+            $posts_count = DB::table('posts')->where('user_id', $user->id)->count();
+            $users = User::all();
+            $likes = 0;
+            $dislikes = 0;
+            foreach ($posts as $post) {
+                foreach ($users as $user_loop) {
+                    $likes += $user_loop->likes()->where('post_id', $post->id)->where('user_id', $user->id)->where('like', 1)->count();
+                    $dislikes += $user_loop->likes()->where('post_id', $post->id)->where('like', 0)->count();
+                }
+                $post->likes = $likes;
+                $post->dislikes = $dislikes;
+                $likes = 0;
+                $dislikes = 0;
+            }
+//        $user = DB::table('users')->where('name', 'John')->first();
+
+            return view('userpage', ['user' => $user, 'posts' => $posts, 'posts_count' => $posts_count, 'likes' => $likes, 'dislikes' => $dislikes]);
+        }
+
+
+        else{
+            $user = User::find($id);
+            $posts = DB::table('posts')->where('user_id', $user->id)->get();
+            $posts_count = DB::table('posts')->where('user_id', $user->id)->count();
+            $users = User::all();
+            $likes = 0;
+            $dislikes = 0;
+            foreach ($posts as $post) {
+                foreach ($users as $user_loop) {
+                    $likes += $user_loop->likes()->where('post_id', $post->id)->where('user_id', $user->id)->where('like', 1)->count();
+                    $dislikes += $user_loop->likes()->where('post_id', $post->id)->where('like', 0)->count();
+                }
+                $post->likes = $likes;
+                $post->dislikes = $dislikes;
+                $likes = 0;
+                $dislikes = 0;
+            }
+//        $user = DB::table('users')->where('name', 'John')->first();
+
+            return view('userpage', ['user' => $user, 'posts' => $posts, 'posts_count' => $posts_count, 'likes' => $likes, 'dislikes' => $dislikes]);
+        }
+    }
+
+
 }
